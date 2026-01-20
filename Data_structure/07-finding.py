b9aed158330ec9bcf82cfa5f62d938815a68fe6e
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-01-20 07:01:12 +0530

    20-01-2026

---
+++ Data_structure/07-finding.py
@@ -0,0 +1,7 @@
+letter = ['a','b','c','d']
+
+for value in letter:
+    print(value)
+    print(letter.index('b'))
+print(letter.count('a'))
+
---
+++ Data_structure/8-sorting.py
@@ -0,0 +1,26 @@
+numbers = [1,4,6,9,10]
+
+print(numbers.sort(reverse=True))
+print(sorted(numbers, reverse=True))
+
+
+
+item = [
+    ('product1', 10),
+    ('product7', 20),
+    ('product8', 90),
+]
+
+for val in numbers:
+    print(val[1])
+    
+    
+def item_sorts(item):
+    return item[1]
+
+item.sort(key=item_sorts)
+print(item)
+
+    
+
+
