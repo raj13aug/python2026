5d3ec40a1d6095aa97038865487eb2990ea0d160
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2026-04-07 20:10:23 +0530

    Update constructor.py

--- class/constructor.py
+++ class/constructor.py
@@ -1,11 +1,24 @@
-class Point:
+# class Point:
+#     def __init__(self,x,y):
+#         self.x = x
+#         self.y = y
+        
+#     def draw(self):
+#         print(f"point ({self.x} {self.y})")
+        
+# point = Point(1, 3) 
+# print(point.x)
+# point.draw()
+
+
+class Hello():
     def __init__(self,x,y):
         self.x = x
         self.y = y
-        
+    
     def draw(self):
-        print(f"point ({self.x} {self.y})")
+        print(f"what is your name {self.x}, which class is studing {self.y}")
         
-point = Point(1, 3) 
-print(point.x)
-point.draw()
+
+z = Hello(1,3)
+z.draw()
